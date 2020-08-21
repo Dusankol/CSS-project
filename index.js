d311f1c9daa1550888f84e91e8dca99cf175aa97
Dusankol <dusank89@gmail.com>
2020-08-21 17:46:59 +0200

    CSS project

---
+++ index.js
@@ -0,0 +1,7 @@
+
+var menu=document.querySelector(".mainMenu");
+var menuToggle=document.querySelector(".toggleMenu");
+
+menuToggle.addEventListener("click",function(){
+  menu.classList.toggle("visible")
+});
